6afddc88139e04e86834b0c87059ef37624069c2
M-adhitya235 <11211053@student.itk.ac.id>
2024-02-15 11:14:16 +0800

    tfm progress

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login/';
+// import Register from './pages/register/';
+
+export default function App() {
+  return (
+    <div className="font-serif">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/register" element={<Register />} /> */}
+        </Routes>
+      </Router>
+    </div>
+  );
+}
